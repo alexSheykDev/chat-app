39055f8dfb078da6783f78ad35d843eedc379b1e
oleksii.sheiko <oleksii.sheiko@otakoyi.com>
2025-01-05 13:04:01 +0200

    feat: established connection with mongodb

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors")
+const mongoose = require('mongoose')
 
 const app = express()
+require('dotenv').config()
 
 app.use(express.json())
 app.use(cors())
 
-const port = 5000;
+// port from the env variable
+const port = process.env.PORT || 5001;
+const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
     console.log(`Server running on port: ${port}`)
 })
+
+mongoose.connect(uri)
+.then(() => console.log("MongoDB connection established"))
+.catch((error) => console.log(`MongoDb connection failed: ${error.message}`))
