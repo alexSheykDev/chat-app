2f8096cc24d7f6ec506c5c8dd6b88722c97c0a06
oleksii.sheiko <oleksii.sheiko@otakoyi.com>
2025-01-20 21:43:49 +0200

    feat: added baseic auth routes and configss

--- server/index.js
+++ server/index.js
@@ -1,21 +1,29 @@
-const express = require("express");
-const cors = require("cors")
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRoute = require('./Routes/userRoute');
 
-const app = express()
-require('dotenv').config()
+const app = express();
+require('dotenv').config();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+app.use('/api/users', userRoute);
+
+//CRUD
+app.get('/', (req, res) => {
+  res.send('Welcome to our chat app API');
+});
 
 // port from the env variable
 const port = process.env.PORT || 5001;
 const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
-    console.log(`Server running on port: ${port}`)
-})
+  console.log(`Server running on port: ${port}`);
+});
 
-mongoose.connect(uri)
-.then(() => console.log("MongoDB connection established"))
-.catch((error) => console.log(`MongoDb connection failed: ${error.message}`))
+mongoose
+  .connect(uri)
+  .then(() => console.log('MongoDB connection established'))
+  .catch((error) => console.log(`MongoDb connection failed: ${error.message}`));
