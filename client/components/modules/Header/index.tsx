9f21335e86f75bf14a024700d1e819ce496adecd
oleksii.sheiko <oleksii.sheiko@otakoyi.com>
2025-06-11 14:18:32 +0300

    feat: made logo clickable

--- client/components/modules/Header/index.tsx
+++ client/components/modules/Header/index.tsx
@@ -1,6 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
 import SettingsIcon from "@/assets/icons/SettingsIcon";
 import ThreeVerticalDotsIcon from "@/assets/icons/ThreeVerticalDotsIcon";
-import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Popover,
@@ -12,9 +13,16 @@ import ProfilePopover from "@/components/modules/Profile/ProfilePopover";
 const Header = () => {
   return (
     <div className="h-16 flex justify-between items-center border-b border-gray-300 px-4">
-      <div>
-        <Image src="/logo-chat.png" width={159} height={56} alt="Logo" />
-      </div>
+      <Link href="/chat">
+        <Image
+          src="/logo-chat.png"
+          width={159}
+          height={56}
+          alt="Logo"
+          className="cursor-pointer"
+        />
+      </Link>
+
       <div className="flex items-center gap-x-4">
         <SettingsIcon />
         <ThreeVerticalDotsIcon />
